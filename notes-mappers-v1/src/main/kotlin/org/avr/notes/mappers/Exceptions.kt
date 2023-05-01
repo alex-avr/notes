290a87039daca28d7df5cd8047c1cfd8f21fd16e
alex <alex.avr@gmail.com>
2023-05-01 16:27:03 +0700

    Реализация бэкенда с использованием Ktor

--- notes-mappers-v1/src/main/kotlin/org/avr/notes/mappers/Exceptions.kt
+++ notes-mappers-v1/src/main/kotlin/org/avr/notes/mappers/Exceptions.kt
@@ -3,6 +3,7 @@ package org.avr.notes.mappers
 import org.avr.notes.common.models.folder.FolderCommand
 import org.avr.notes.common.models.note.NoteCommand
 
+class MissingRequestParameterException(message: String) : RuntimeException(message)
 class UnknownRequestException(clazz: Class<*>) : RuntimeException("Class $clazz can't be mapped and not supported")
 class UnknownNoteCommandException(command: NoteCommand) : RuntimeException("Unknown command for note: $command")
 class UnknownFolderCommandException(command: FolderCommand) : RuntimeException("Unknown command for folder: $command")
