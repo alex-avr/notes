b88f3d82655a7b409b81977e9754a82508db3009
alex <alex.avr@gmail.com>
2023-05-07 12:19:39 +0700

    Документирование

--- notes-api-v1-jackson/src/main/kotlin/org/avr/notes/app/v1/FolderRequestParameters.kt
+++ notes-api-v1-jackson/src/main/kotlin/org/avr/notes/app/v1/FolderRequestParameters.kt
@@ -1,5 +1,11 @@
 package org.avr.notes.app.v1
 
+/**
+ * Возможные параметры запроса для папок
+ */
 data class FolderRequestParameters(
+    /**
+     * Идентификатор папки
+     */
     val folderId: String? = null
 )
